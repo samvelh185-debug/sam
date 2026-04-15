a5d18ff554f61d4d468c93e9c1711563145eefcc
Samo <samvelh185@gmail.com>
2026-04-15 21:25:37 +0400

    i added dummy console.log

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const server = http.createServer (async function (req,res){
     res.setHeader('Content-Type','text/plain');
     res.end();
 })
+console.log(1);
 console.log(PORT);
 server.listen(PORT,hostname,()=>{
     console.log(`server is runing on ${PORT}`);
