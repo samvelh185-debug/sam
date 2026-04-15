7320dda5e1fa7dad07fee3018c7e64f33a9c7359
Samo <samvelh185@gmail.com>
2026-04-15 21:15:01 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import http from 'http';
+import 'dotenv/config';
+const hostname = "localhost";
+const {PORT}=process.env;
+const server = http.createServer (async function (req,res){
+     res.statusCode =200;
+    res.setHeader('Content-Type','text/plain');
+    res.end();
+})
+console.log(PORT);
+server.listen(PORT,hostname,()=>{
+    console.log(`server is runing on ${PORT}`);
+})
